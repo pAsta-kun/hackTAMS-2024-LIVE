d668992ae09206fb0cc51de71451a53177f7ba62
ali v <112457969+pAsta-kun@users.noreply.github.com>
2024-01-15 13:39:16 -0600

    basically done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function scheduleSwitch() {
     for(let i = 0; i < items.length; i++)
     {
         items[i].style.display = "none";
+        items[i].classList.remove('show');
     }
     if(value === '.in-person')
     {
@@ -19,4 +20,28 @@ function scheduleSwitch() {
     {
         items[i].style.display = "flex";
     }
+}
+
+function intialHide() {
+    let items = document.querySelectorAll('.virtual')
+    for(let i = 0; i < items.length; i++)
+    {
+        items[i].style.display = "none";
+    }
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((e1) => observer.observe(e1));
+
+function test() {
+    console.log('test')
 }
